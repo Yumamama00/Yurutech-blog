9c985e6e47e5913cd78231e59e70185b380daad7
YumaIshida00 <yu_basket11@icloud.com>
2022-05-03 16:36:39 +0900

    文言、スタイル微修正

--- components/TOCInline.tsx
+++ components/TOCInline.tsx
@@ -39,7 +39,7 @@ const TOCInline = ({
   return (
     <>
       {asDisclosure ? (
-        <details open className="mb-4">
+        <details open className="mb-6">
           <summary className="ml-6 py-2 text-xl font-bold">目次</summary>
           <div className="ml-6">{tocList}</div>
         </details>
