f5befe4678271fbbc4fa0b7ec12361a077e4c9ae
YumaIshida00 <yu_basket11@icloud.com>
2022-05-07 15:33:23 +0900

    update: 404 Page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,24 +1,33 @@
 import Link from '@/components/Link'
+import { PageSEO } from '@/components/SEO'
+import siteMetadata from '@/data/siteMetadata'
 
 export default function FourZeroFour() {
   return (
-    <div className="my-auto flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
-      <div className="space-x-2 pt-6 pb-8 md:space-y-5">
-        <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:border-r-2 md:px-6 md:text-8xl md:leading-14">
-          404
-        </h1>
-      </div>
+    <>
+      <PageSEO
+        title={`Page not found | ${siteMetadata.title}`}
+        description="お探しのページは見つかりませんでした。"
+      ></PageSEO>
 
-      <div className="max-w-md">
-        <p className="mb-4 text-xl font-bold leading-normal md:text-xl">
-          お探しのページは見つかりませんでした。
-        </p>
-        <Link href="/">
-          <button className="focus:shadow-outline-primary inline rounded-lg border border-transparent bg-primary-500 px-4 py-2 text-sm font-semibold leading-5 text-white shadow transition-colors duration-150 hover:bg-primary-700 focus:outline-none dark:hover:bg-primary-500">
-            ホーム画面に戻る
-          </button>
-        </Link>
+      <div className="my-auto flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
+        <div className="space-x-2 pt-6 pb-8 md:space-y-5">
+          <span className="text-5xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:border-r-2 md:px-6 md:text-6xl md:leading-14">
+            404
+          </span>
+        </div>
+
+        <div className="max-w-md">
+          <p className="mb-4 text-xl font-bold leading-normal md:text-xl">
+            お探しのページは見つかりませんでした。
+          </p>
+          <Link href="/">
+            <button className="focus:shadow-outline-primary inline rounded-lg border border-transparent bg-primary-500 px-4 py-2 text-sm font-semibold leading-5 text-white shadow transition-colors duration-150 hover:bg-primary-700 focus:outline-none dark:hover:bg-primary-500">
+              ホーム画面に戻る
+            </button>
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
